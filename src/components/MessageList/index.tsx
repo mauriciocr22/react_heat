f06812dfb50a09e9c95793f5a8b4ba70f97d76f2
Maurício de Carvalho <mauriciocr223@gmail.com>
2021-10-27 20:53:50 -0300

    feat: message list component

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,43 @@
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+
+export function MessageList() {
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImg}>
+              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImg}>
+              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImg}>
+              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
